daaa0b7ad8c8ff695aede851f0da1adeaf108ecb
AbdussamadYisau <sammieyisau@gmail.com>
2021-01-30 21:19:01 +0100

    Fixed Base Route json response

--- controllers/baseRouteController.js
+++ controllers/baseRouteController.js
@@ -1,5 +1,4 @@
 exports.getBase = (req,res,next) => {
-    if((req.status === 200||201, req.statusText === 'OK')) {
         return res.json({
             "message": "My Rule-Validation API",
             "status": "success",
@@ -11,10 +10,5 @@ exports.getBase = (req,res,next) => {
                 "twitter": "@sammieyisau_"
             }
         });
-    } else {
-        return res.status(404 || 501).json({
-            "status" : "error",
-            "message" : "Unable to communicate with database"
-        });
-    }
+    
 }
